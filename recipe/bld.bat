54850c4d450f11876d283ad3168956d70ac7c71e
Marcel Bargull <marcel.bargull@udo.edu>
2021-10-08 13:29:52 +0200

    Fix version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,8 @@
 @echo ON
 setlocal enabledelayedexpansion
 
+ >.version echo VERSION=%PKG_VERSION%
+>>.version echo RELEASE=1
+>>.version echo FULL=%PKG_VERSION%
 make install PREFIX=%LIBRARY_PREFIX% LCOV_PERL_PATH=
 cp %RECIPE_DIR%\lcov.bat %LIBRARY_BIN%
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,9 @@
 
 set +x
 
+>.version cat <<EOF
+VERSION=${PKG_VERSION}
+RELEASE=1
+FULL=${PKG_VERSION}
+EOF
 make install PREFIX=$PREFIX LCOV_PERL_PATH=
